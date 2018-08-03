759a40c0dccd8b999d49c2b099ecfaf40dd1364c
Lee Simon <shinoblee@gmail.com>
2018-08-03 13:08:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+let recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object.{key: 'value'};
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
